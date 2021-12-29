efe4f5d20ee96e5d434890c90a067d0f17ea8f33
lee <rhsoddld@hotmail.com>
2021-12-29 18:58:15 +0900

    Angular base frontpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+// import { ProductListComponent } from './product/product-list/product-list.component';
+// import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+// --> product.module.ts
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    // ProductListComponent, --> product.module.ts
+    // ProductDetailComponent, --> product.module.ts
+    // ProductComponent
   ],
   imports: [
     BrowserModule,
